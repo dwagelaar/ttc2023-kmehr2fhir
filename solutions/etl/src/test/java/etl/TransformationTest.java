bcde60cd5f0944e784f00b2ad9827a1b0c77ef00
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-07-04 14:02:40 +0100

    Add parameterized test and switch to Epsilon interims
    There is an important optimisation pending to be merged in Epsilon:
    
    https://github.com/eclipse/epsilon/pull/44
    
    This will end up in the 2.5.0 interim versions, so this commit
    changes the solution to use those.

--- solutions/etl/src/test/java/etl/TransformationTest.java
+++ solutions/etl/src/test/java/etl/TransformationTest.java
@@ -2,18 +2,22 @@ package etl;
 
 import java.io.File;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import ttc2023.kmehr2fhir.etl.Transformation;
 
 public class TransformationTest {
 
 	private static final String MODELS_SUMEHR_EXAMPLE_FHIR = "etl-sumehr_example.fhir";
-	private static final String MODELS_SUMEHR_EXAMPLE_KMEHR = "../../models/sumehr_example.kmehr";
+
+	public static File[] inputFiles() {
+		return new File("../../models").listFiles(f -> f.getName().endsWith(".kmehr"));
+	}
 	
-	@Test
-	public void runTransformation() throws Exception {
-		final File fKmehr = new File(MODELS_SUMEHR_EXAMPLE_KMEHR);
+	@ParameterizedTest
+	@MethodSource("inputFiles")
+	public void runTransformation(File fKmehr) throws Exception {
 		final File fFhir = new File(MODELS_SUMEHR_EXAMPLE_FHIR);
 		new Transformation(fKmehr, fFhir).run();
 	}
